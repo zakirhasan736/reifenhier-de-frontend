0a1758ac033b24176e306112dd173e8898bfdd7e
zakirhossaib736 <zakirhossaib736@gmail.com>
2025-07-26 21:51:59 +0600

    updated details page and public/images/vendor image

--- src/page-components/products/ProductsListing.tsx
+++ src/page-components/products/ProductsListing.tsx
@@ -1,18 +1,11 @@
 'use client';
 
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import ProductBanner from '@/components/productpage/ProductBanner';
 import ProductListingsSec from '@/components/productpage/ProductListingsSec';
-// import type { RootState } from '@/store/store'; 
 
 const ProductsListing = () => {
-  // const filters = useSelector((state: RootState) => state.products.filters);
-  // const selectedCategory = filters?.category?.[0] || 'All Products';
-
   return (
     <div className="product-listing-main-wrapper">
-      {/* <ProductBanner categoryName={selectedCategory} /> */}
       <ProductListingsSec />
     </div>
   );
