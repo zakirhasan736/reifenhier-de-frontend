5f997edf3a6f19b17b0a67237ae89d33cfffb1a1
zakirhossaib736 <zakirhossaib736@gmail.com>
2025-11-23 00:14:54 +0600

    updated banner

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -30,13 +30,12 @@ const BannerSection = () => {
             <div className="tyres-search-content w-full relative">
               <div className="tyres-search-left-content lg:mb-10 md:mb-8 mb-7  relative">
                 <h1 className="h2 text-mono-0 text-center mb-2 md:mb-4 md:!text-[38px] lg:!text-[48px] leading-[115%] font-medium font-primary !text-[26px]">
-                  Finden Sie die perfekten Reifen{' '}
+                  Finden Sie den perfekten Reifen{' '}
                   <br className="max-sm:hidden" />
-                  für Ihr Fahrzeug
+                  für Ihr Fahrzeug und zum besten Preis
                 </h1>
                 <p className="text-[#FFFFFFCC] lg:text-[20px] md:text-[18px] text-[16px] text-center font-normal font-primary leading-[150%]">
-                  Sorgen Sie schnell für Reifen, die zu Ihrem Fahrzeug und
-                  Fahrstil passen.
+                  Über 40000 Reifen mehrmals täglich für Sie aktualisiert.
                 </p>
               </div>
               <FilterForm />
